998b39cb784525f31474dad04b36ffbe17f46132
artav <artav.work@outlook.com>
2022-01-18 20:23:39 +0330

    Add project files.

---
+++ Mafia/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace Mafia
+{
+	public partial class MainPage  : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Mafia/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mafia
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+
+            builder
+                .RegisterBlazorMauiWebView()
+                .UseMauiApp<App>();
+
+//            builder.Services.ConfigureServices();
+
+            builder.Services.AddBlazorWebView();
+
+            return builder.Build();
+        }
+    }
+}
